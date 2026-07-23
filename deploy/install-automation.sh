4eb60877dfbd773cf02f42bd6b3c16977e048df0
petrkotab666 <petrkotab@seznam.cz>
2026-07-23 15:37:28 +0200

    Doplňovat favicon při každém nasazení

--- deploy/install-automation.sh
+++ deploy/install-automation.sh
@@ -22,6 +22,7 @@ chown -R ubuntu:ubuntu "$APP_DIR"
 su - ubuntu -c "git -C '$APP_DIR' fetch origin && git -C '$APP_DIR' reset --hard origin/main"
 python3 "$APP_DIR/scripts/update_events.py"
 python3 "$APP_DIR/scripts/update_sports.py"
+python3 "$APP_DIR/scripts/ensure_favicon.py"
 rsync -a --delete \
   --exclude='.git' --exclude='.github' --exclude='deploy' \
   --exclude='Dockerfile' --exclude='docker-compose.yml' --exclude='nginx' \
