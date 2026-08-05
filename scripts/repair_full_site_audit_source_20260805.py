800d8698a305e7f8c5c32ec5888e211faf7edba1
petrkotab666 <petrkotab@seznam.cz>
2026-08-05 22:24:56 +0200

    Opravit syntaxi úplného auditu webu

--- scripts/repair_full_site_audit_source_20260805.py
+++ scripts/repair_full_site_audit_source_20260805.py
@@ -3,15 +3,25 @@ from pathlib import Path
 
 path = Path(__file__).with_name("full_site_audit_20260805.py")
 text = path.read_text(encoding="utf-8")
+
+# Starší verze auditu obsahovala několik raw řetězců uzavřených apostrofem,
+# přestože regulární výraz současně obsahoval znak '. Přepiš je bezpečně
+# na raw řetězce uzavřené trojitými uvozovkami.
 replacements = {
-    "r'<link\\b[^>]*rel=[\\\"']canonical[\\\"'][^>]*href=[\\\"']([^\\\"']+)'": "r\"<link\\b[^>]*rel=[\\\"']canonical[\\\"'][^>]*href=[\\\"']([^\\\"']+)\"",
-    "r'href=[\\\"']([^\\\"']*?/clanky/[^\\\"']+\\.html)'": "r\"href=[\\\"']([^\\\"']*?/clanky/[^\\\"']+\\.html)\"",
-    "r'href=[\\\"'](/clanky/strana-\\d+\\.html)[\\\"']'": "r\"href=[\\\"'](/clanky/strana-\\d+\\.html)[\\\"']\"",
-    "r'<a\\b[^>]*class=[\\\"'][^\\\"']*\\bread-more\\b[^\\\"']*[\\\"'][^>]*href=[\\\"']([^\\\"']+)'": "r\"<a\\b[^>]*class=[\\\"'][^\\\"']*\\bread-more\\b[^\\\"']*[\\\"'][^>]*href=[\\\"']([^\\\"']+)\"",
-    "r'href=[\\\"']([^\\\"']+)'": "r\"href=[\\\"']([^\\\"']+)\"",
-    "r'<article\\b[^>]*class=[\\\"'][^\\\"']*\\barticle-card\\b[^\\\"']*[\\\"'][^>]*>.*?</article>'": "r\"<article\\b[^>]*class=[\\\"'][^\\\"']*\\barticle-card\\b[^\\\"']*[\\\"'][^>]*>.*?</article>\"",
+    "return re.findall(r'<article\\b[^>]*class=[\\\"'][^\\\"']*\\barticle-card\\b[^\\\"']*[\\\"'][^>]*>.*?</article>', text, re.I | re.S)":
+    "return re.findall(r'''<article\\b[^>]*class=[\\\"'][^\\\"']*\\barticle-card\\b[^\\\"']*[\\\"'][^>]*>.*?</article>''', text, re.I | re.S)",
+    "re.search(r'<link\\b[^>]*rel=[\\\"']canonical[\\\"'][^>]*href=[\\\"']([^\\\"']+)', text, re.I)":
+    "re.search(r'''<link\\b[^>]*rel=[\\\"']canonical[\\\"'][^>]*href=[\\\"']([^\\\"']+)''', text, re.I)",
+    "re.findall(r'href=[\\\"']([^\\\"']*?/clanky/[^\\\"']+\\.html)', archive_text, re.I)":
+    "re.findall(r'''href=[\\\"']([^\\\"']*?/clanky/[^\\\"']+\\.html)''', archive_text, re.I)",
+    "re.findall(r'href=[\\\"'](/clanky/strana-\\d+\\.html)[\\\"']', text, re.I)":
+    "re.findall(r'''href=[\\\"'](/clanky/strana-\\d+\\.html)[\\\"']''', text, re.I)",
+    "re.search(r'<a\\b[^>]*class=[\\\"'][^\\\"']*\\bread-more\\b[^\\\"']*[\\\"'][^>]*href=[\\\"']([^\\\"']+)', block, re.I)":
+    "re.search(r'''<a\\b[^>]*class=[\\\"'][^\\\"']*\\bread-more\\b[^\\\"']*[\\\"'][^>]*href=[\\\"']([^\\\"']+)''', block, re.I)",
+    "re.findall(r'href=[\\\"']([^\\\"']+)', block, re.I)":
+    "re.findall(r'''href=[\\\"']([^\\\"']+)''', block, re.I)",
 }
 for old, new in replacements.items():
     text = text.replace(old, new)
 path.write_text(text, encoding="utf-8", newline="\n")
-print("Zdroj úplného auditu byl syntakticky normalizován.")
+print("Zdroj úplného auditu byl syntakticky opraven.")
